d46c5902cfa5cc28ca15b175dd6339954ef705ce
Leonardo Viva <leonardo.leal@konecty.com>
2021-07-05 17:20:08 -0300

    Update data server-side fn

--- src/components/Cards/study.js
+++ src/components/Cards/study.js
@@ -1,6 +1,5 @@
 import React, { useState, useCallback } from "react";
 
-import Box from "@material-ui/core/Box";
 import Grid from "@material-ui/core/Grid";
 import Card from "@material-ui/core/Card";
 import CardContent from "@material-ui/core/CardContent";
