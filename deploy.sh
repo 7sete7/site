8f77ec480b6bde89394a35a66c088874271ca48c
Leonardo Viva <leonardo.leal@konecty.com>
2022-09-20 20:05:46 -0300

    fix: fix deploy script

--- deploy.sh
+++ deploy.sh
@@ -6,10 +6,6 @@ npm run build
 # Clean node_modules branch cache
 rm -rf node_modules/.cache/gh-pages
 
-# Transform absolute paths to relative for it to work on gh-pages
-# ex: <srcipt src="/index.js"> ===> <srcipt src="index.js">
-sed -i -E 's/([src|href]+=")\//\1/g' dist/index.html
-
 # Deploy to gh-pages
 currentdate=`date +"%d/%m"`
-NODE_DEBUG=gh-pages node_modules/.bin/gh-pages --dist dist --message "New release: $currentdate"
+NODE_DEBUG=gh-pages node_modules/.bin/gh-pages --dist build --message "New release: $currentdate"
